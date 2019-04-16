e3d0ec8212f839e7e0f01159eac2e906b62545e8
successive-dev <vishal.tewatia@successive.tech>
2019-04-16 17:40:41 +0530

    fixed untracked files

--- build/src/controllers/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user_1 = require("./user");
-exports.user = user_1.user;
-exports.userRouter = user_1.userRouter;
--- build/src/repositories/versionable/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var VersionableRepository_1 = require("./VersionableRepository");
-exports.VersionableRepository = VersionableRepository_1.default;
-var VersionableSchema_1 = require("./VersionableSchema");
-exports.VersionableSchema = VersionableSchema_1.default;
