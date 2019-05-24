0f2570f1d9e5eb80c759e90a5c687ffe2fa024b2
dev-successive <vishal.tewatia@successive.tech>
2019-05-24 18:59:34 +0530

    more changes

--- brigade.js
+++ brigade.js
@@ -2,13 +2,8 @@ const { events, Job } = require("brigadier");
 events.on("push", () => {
   var job = new Job("do-nothing", "alpine:3.8");
   job.tasks = [
-    "echo Hello",
-    "echo World",
-    "echo Vishal",
-    "echo random stuff",
-    "echo Tewatia",
-    "echo more changes",
-    "echo even more changes",
+    "cd /src",
+    "ls -lart"
   ];
 
   job.run();
