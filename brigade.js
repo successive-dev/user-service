5aabb4454534ea4072abe3d755c9753e81315fdf
dev-successive <vishal.tewatia@successive.tech>
2019-05-27 19:38:47 +0530

    some changes

--- brigade.js
+++ brigade.js
@@ -1,6 +1,7 @@
 const { events, Job } = require("brigadier");
 events.on("push", () => {
-  var job = new Job("lint-check", "node");
+  let job = new Job("lint-check", "node");
+  let deployjob = new Job("Deploy-job", "docker");
   job.tasks = [
     "cd /src",
     "ls -lart",
@@ -8,13 +9,13 @@ events.on("push", () => {
     "npm i",
     "npm run lint:fix",    
   ];
-  var deployjob = new Job("Deploy-job", "docker");
   deployjob.tasks = [
     "docker build -t nxvishal/user-service:base .",
     "docker login -u nxvishal -p wJD87CnY45n5Lar",
     "docker push nxvishal/user-service:base"
   ]
 
-  job.run();
-  deployjob.run();
+  job.run().then(()=>{
+    deployjob.run();
+  })
 });
