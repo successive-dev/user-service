e9a57afb60686e189a85e8648c5641b1144124c6
dev-successive <vishal.tewatia@successive.tech>
2019-05-27 20:42:45 +0530

    some changes

--- brigade.js
+++ brigade.js
@@ -12,6 +12,7 @@ events.on("push", async () => {
       "npm run lint:fix",    
     ];
     j2.tasks = [
+      "ll",
       "docker build -t nxvishal/user-service:base .",
       "docker login -u nxvishal -p wJD87CnY45n5Lar",
       "docker push nxvishal/user-service:base",
