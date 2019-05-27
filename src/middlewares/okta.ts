dd0ed1c7267c922b9b407a249f20ba6b98d6b984
dev-successive <vishal.tewatia@successive.tech>
2019-05-27 20:26:18 +0530

    some changes

--- src/middlewares/okta.ts
+++ src/middlewares/okta.ts
@@ -15,16 +15,16 @@
 // import { configuration } from '../config';
 
 import { NextFunction, Response } from 'express';
-const OktaJwtVerifier = require('@okta/jwt-verifier');
+// const OktaJwtVerifier = require('@okta/jwt-verifier');
 
-const oktaJwtVerifier = new OktaJwtVerifier({
-  clientId: '0oafvaqmsRtxLk6nS356',
-  issuer: 'https://dev-246708.okta.com/oauth2/default',
-// tslint:disable-next-line: object-literal-sort-keys
-  assertClaims: {
-    aud: 'api://default',
-  },
-});
+// const oktaJwtVerifier = new OktaJwtVerifier({
+//   clientId: '0oafvaqmsRtxLk6nS356',
+//   issuer: 'https://dev-246708.okta.com/oauth2/default',
+// // tslint:disable-next-line: object-literal-sort-keys
+//   assertClaims: {
+//     aud: 'api://default',
+//   },
+// });
 
 function authenticationRequired(req: any, res: Response, next: NextFunction) {
   const authHeader = req.headers.authorization || '';
@@ -34,14 +34,14 @@ function authenticationRequired(req: any, res: Response, next: NextFunction) {
   }
   const accessToken = match[1];
 
-  return oktaJwtVerifier.verifyAccessToken(accessToken)
-    .then((jwt: any) => {
-      req.jwt = jwt;
-      next();
-    })
-    .catch((err: Error) => {
-      res.status(401).send(err.message);
-    });
+  // return oktaJwtVerifier.verifyAccessToken(accessToken)
+  //   .then((jwt: any) => {
+  //     req.jwt = jwt;
+  //     next();
+  //   })
+  //   .catch((err: Error) => {
+  //     res.status(401).send(err.message);
+  //   });
 }
 
 export default authenticationRequired;
