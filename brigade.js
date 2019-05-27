3e713d2b632276b57711f0f9841109eaefdf3df7
dev-successive <vishal.tewatia@successive.tech>
2019-05-27 20:12:06 +0530

    some changes

--- brigade.js
+++ brigade.js
@@ -2,7 +2,7 @@ const { events, Job } = require("brigadier");
 events.on("push", async () => {
   try {
     let j1 = new Job("lint-check", "node");
-    let j2 = new Job("Deploy-job", "docker");
+    // let j2 = new Job("Deploy-job", "docker");
 
     j1.tasks = [
       "cd /src",
@@ -11,14 +11,14 @@ events.on("push", async () => {
       "npm i",
       "npm run lint:fix",    
     ];
-    j2.tasks = [
-      // "docker build -t nxvishal/user-service:base .",
-      // "docker login -u nxvishal -p wJD87CnY45n5Lar",
-      // "docker push nxvishal/user-service:base"
-      "ls"
-    ];
+    // j2.tasks = [
+    //   // "docker build -t nxvishal/user-service:base .",
+    //   // "docker login -u nxvishal -p wJD87CnY45n5Lar",
+    //   // "docker push nxvishal/user-service:base"
+    //   "ls"
+    // ];
     await j1.run();
-    await j2.run();
+    // await j2.run();
   } catch (error) {
     console.log(error.message);
     console.log(error.lineNumber);
