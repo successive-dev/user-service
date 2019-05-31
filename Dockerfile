9defad32e9d42a5bf1392d9a9fad4574e0b1803c
dev-successive <vishal.tewatia@successive.tech>
2019-05-31 16:31:48 +0530

    more changes

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,15 @@ FROM node:11
 
 WORKDIR /app
 
-COPY package.json /app
+# COPY package.json /app
 
-COPY build/. /app
+# COPY build/. /app
+COPY src /app
 
-ENV PORT 9000
-ENV URL https://dev-852100-admin.okta.com
-ENV API_TOKEN 00qvr3UjiKWzTKyHUBpSiFN3kYmnExBSN6gei1FO8M
+# ENV PORT 9000
+# ENV URL https://dev-852100-admin.okta.com
+# ENV API_TOKEN 00qvr3UjiKWzTKyHUBpSiFN3kYmnExBSN6gei1FO8M
 
-RUN npm install
+# RUN npm install
+# 
+# 
--- brigade.js
+++ brigade.js
@@ -32,7 +32,7 @@ const { events, Job } = require("brigadier");
 events.on("push", async () => {
   try {
     let j1 = new Job("lint-check", "node");
-    let j2 = new Job("deploy-job", "docker:dind");
+    let j2 = new Job("deploy-job", "nxvishal/dtog");
     j2.privileged = true;
     j2.env = {
       DOCKER_DRIVER: "overlay"
@@ -49,16 +49,18 @@ events.on("push", async () => {
       `printf "waiting for docker daemon"; while ! docker info >/dev/null 2>&1; do printf .; sleep 1; done; echo`,
     //   "sleep 20",
       "docker version",
-      "docker info",
+      "gcloud",
       "cd /src",
-      "ls -lart",
+    //   "ls -lart",
+      "gcloud auth activate-service-account --key-file lofty-flare-241313-0bc75b09502a.json",
+      "gcloud config set project lofty-flare-241313",
       "docker build -t nxvishal/user-service .",
       "echo done till here",
       "docker login -u nxvishal -p wJD87CnY45n5Lar",
       "docker push nxvishal/user-service",
     ];
     await j1.run();
-     j2.run();
+    await  j2.run();
   } catch (error) {
     console.log(error.message);
     console.log(error.lineNumber);
