3628328a001c1cd8374d8c086bbb3420ed593d96
dev-successive <vishal.tewatia@successive.tech>
2019-06-05 17:42:40 +0530

    more changes

--- brigade.js
+++ brigade.js
@@ -43,9 +43,9 @@ events.on("push", async (e, project) => {
     j2.tasks = [
       "dockerd-entrypoint.sh &",
       `printf "waiting for docker daemon"; while ! docker info >/dev/null 2>&1; do printf .; sleep 1; done; echo`,
-    //   "sleep 20",
+      // "sleep 20",
       "docker version",
-      // "gcloud",
+      "gcloud",
       "cd /src",
       "echo $KEY > key.json",
       // "apk add jq",
