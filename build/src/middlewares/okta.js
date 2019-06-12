68009cb46d5c5279d9eca1052449335ab7147c35
dev-successive <vishal.tewatia@successive.tech>
2019-06-12 17:15:24 +0530

    pipeline done

--- build/src/middlewares/okta.js
+++ build/src/middlewares/okta.js
@@ -1,15 +1,15 @@
 "use strict";
 // const { ExpressOIDC } = require('@okta/oidc-middleware');
 Object.defineProperty(exports, "__esModule", { value: true });
-var OktaJwtVerifier = require('@okta/jwt-verifier');
-var oktaJwtVerifier = new OktaJwtVerifier({
-    clientId: '0oafvaqmsRtxLk6nS356',
-    issuer: 'https://dev-246708.okta.com/oauth2/default',
-    // tslint:disable-next-line: object-literal-sort-keys
-    assertClaims: {
-        aud: 'api://default',
-    },
-});
+// const OktaJwtVerifier = require('@okta/jwt-verifier');
+// const oktaJwtVerifier = new OktaJwtVerifier({
+//   clientId: '0oafvaqmsRtxLk6nS356',
+//   issuer: 'https://dev-246708.okta.com/oauth2/default',
+// // tslint:disable-next-line: object-literal-sort-keys
+//   assertClaims: {
+//     aud: 'api://default',
+//   },
+// });
 function authenticationRequired(req, res, next) {
     var authHeader = req.headers.authorization || '';
     var match = authHeader.match(/Bearer (.+)/);
@@ -17,13 +17,13 @@ function authenticationRequired(req, res, next) {
         return res.status(401).send('Error: No authHeaders').end();
     }
     var accessToken = match[1];
-    return oktaJwtVerifier.verifyAccessToken(accessToken)
-        .then(function (jwt) {
-        req.jwt = jwt;
-        next();
-    })
-        .catch(function (err) {
-        res.status(401).send(err.message);
-    });
+    // return oktaJwtVerifier.verifyAccessToken(accessToken)
+    //   .then((jwt: any) => {
+    //     req.jwt = jwt;
+    //     next();
+    //   })
+    //   .catch((err: Error) => {
+    //     res.status(401).send(err.message);
+    //   });
 }
 exports.default = authenticationRequired;
