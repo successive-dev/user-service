bf01ad8026440e4555c86b839be2a5f769bbf7fc
dev-successive <vishal.tewatia@successive.tech>
2019-06-12 13:31:26 +0530

    pipeline done

--- brigade.js
+++ brigade.js
@@ -52,6 +52,7 @@ events.on("push", async (e, project) => {
       "echo ==================================",
       "gcloud auth configure-docker",
       "rm key.json",
+      "npm start",
       "docker build -t user-service .",
       "docker tag user-service gcr.io/inner-catfish-242312/user-service:$version",
       "echo done till here",
