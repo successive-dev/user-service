c40b7047c2a94e56264fd410729d9d310dd02225
dev-successive <vishal.tewatia@successive.tech>
2019-06-17 19:37:09 +0530

    brigade-trying

--- brigade.js
+++ brigade.js
@@ -32,9 +32,9 @@ events.on("push", async (e, project) => {
     // j1.tasks = [
     //   "cd /mnt/brigade/share",
     //   "echo hello_world > hello_world.txt",
-    //   // "apt install python",
-    //   // "npm i",
-    //   // "npm run lint:fix",    
+    //   "apt install python",
+    //   "npm i",
+    //   "npm run lint:fix",    
     // ];
     j2.tasks = [
 
@@ -43,25 +43,25 @@ events.on("push", async (e, project) => {
       `printf "waiting for docker daemon"; while ! docker info >/dev/null 2>&1; do printf .; sleep 1; done; echo`,
       "cd /src",
       "echo project  " + project.secrets.type,
-      // "cd /mnt/brigade/share",
-      // "cat hello_world.txt",
+      "cd /mnt/brigade/share",
+      "cat hello_world.txt",
 
-      // get version of repo
+      // // get version of repo
       // "git fetch --tags -q",
       // "wget -q -O gitversion https://github.com/screwdriver-cd/gitversion/releases/download/v1.1.1/gitversion_linux_amd64",
       // "chmod u+x ./gitversion",
       // "./gitversion  bump auto && ./gitversion show > pipeline_app_version.txt",
       // "version=$(cat pipeline_app_version.txt)",
 
-      // git authentication
+      // // git authentication
       // 'echo https://successive-dev:uzL623NGxG2Nz9z@github.com > .git-credentials',
       // "git config credential.helper 'store --file .git-credentials'",
 
-      // push tags
+      // // push tags
       // "git remote add origin https://github.com/successive-dev/user-service",
       // "git push origin --tags",
 
-      // create key.json and google auth
+      // // create key.json and google auth
       // "echo $KEY > key.json",
       // "gcloud auth activate-service-account --key-file key.json",
       // "gcloud config set project inner-catfish-242312",
@@ -69,23 +69,23 @@ events.on("push", async (e, project) => {
       // "gcloud config list",
       // "echo ==================================",
 
-      // remove key
+      // // remove key
       // "rm key.json",
 
-      // generating build
+      // // generating build
       // "apk add npm",
       // "npm install",
       // "npm i nodemon",
       // "npm run build",
 
-      // generating, tagging and pushing the image
+      // // generating, tagging and pushing the image
       // "gcloud auth configure-docker",
       // "docker build -t user-service .",
       // "docker tag user-service gcr.io/inner-catfish-242312/user-service:$version",
       // "echo done till here",
       // "docker push gcr.io/inner-catfish-242312/user-service:$version",
 
-      // updating helm chart with latest version of build image
+      // // updating helm chart with latest version of build image
       // "helm init --client-only",
       // "helm ls",
       // "helm repo add usc https://successive-dev.github.io/usc/",
