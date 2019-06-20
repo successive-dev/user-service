bdbca7cd17c31f1e10bdf90a8c5f82922c7eee26
dev-successive <vishal.tewatia@successive.tech>
2019-06-20 12:29:18 +0530

    test

--- deployer/TaskCollection.js
+++ deployer/TaskCollection.js
@@ -16,7 +16,18 @@ class TaskCollection {
 	};
 
 	googleLogin() {
-		
+		// return [
+		// 	type : project.secrets.type,
+    //   project_id : project.secrets.project_id,
+    //   private_key_id : project.secrets.private_key_id,
+    //   private_key : project.secrets.private_key,
+    //   client_email : project.secrets.client_email,
+    //   client_id : project.secrets.client_id,
+    //   auth_uri : project.secrets.auth_uri,
+    //   token_uri : project.secrets.token_uri,
+    //   auth_provider_x509_cert_url : project.secrets.auth_provider_x509_cert_url,
+    //   client_x509_cert_url : project.secrets.client_x509_cert_url,
+		// ]
 	}
 
 
