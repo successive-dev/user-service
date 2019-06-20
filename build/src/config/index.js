77340cc428cd42683303b9f98346d5a77f7a3499
dev-successive <vishal.tewatia@successive.tech>
2019-06-20 12:20:39 +0530

    fixed untracked files

--- build/src/config/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var configuration_1 = require("./configuration");
-exports.configuration = configuration_1.default;
--- build/src/middlewares/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var okta_1 = require("./okta");
-exports.authenticationRequired = okta_1.default;
