d7f13a9ad4940ff3f4c28e127d7c40a471926c4f
dev-successive <vishal.tewatia@successive.tech>
2019-06-21 11:42:31 +0530

    test

--- brigade.js
+++ brigade.js
@@ -51,7 +51,7 @@ events.on("push", async (e, project) => {
       `${project.secrets} | eat | jq '.type'`,
       "echo echoing secrets",
       `echo ${project.secrets} > secrets.json`,
-      "cat secrets.json",
+      "cat secrets.json | eat | fx .type",
       // "echo echoing secrets.json file",
       // "cat secrets.json",
       // "cd /mnt/brigade/share",
