01a9d7195e5140e4d5317a167c42460b6da05584
dev-successive <vishal.tewatia@successive.tech>
2019-07-01 13:46:37 +0530

    test

--- brigade.js
+++ brigade.js
@@ -5,11 +5,11 @@ let TaskCollection = require('./deployer');
 events.on("push", async (e, project) => {
   try {
     let tc = new TaskCollection(e, project);
-    console.log("================================", e, "================================");
-    console.log("================================", project, "================================");
+    // console.log("================================", e, "================================");
+    // console.log("================================", project, "================================");
     var jsonPayload = JSON.parse(e.payload);
-    console.log(typeof (project.secrets));
-    console.log(project.secrets);
+    // console.log(typeof (project.secrets));
+    // console.log(project.secrets);
     const keys = {
       type: project.secrets.type,
       project_id: project.secrets.project_id,
@@ -28,6 +28,25 @@ events.on("push", async (e, project) => {
         tag: '$version',
       },
     }
+
+    let pipeline = [
+      ...tc.dockerInit(),
+      "cd /src",
+      ...tc.gitLogin(),
+      ...tc.gitVersion(),
+      ...tc.googleLogin(),
+      "npm install",
+      "npm i nodemon",
+      "npm run build",
+      ...tc.buildImage('user-service-n'),
+      ...tc.tagAndPush(),
+      "echo done",
+      ...tc.helmInit(),
+      ...tc.helmAddRepo('https://successive-dev.github.io/usc/', 'usc'),
+      ...tc.helmUpgrade('usc', 'usc/user-service', values)
+    ];
+
+    console.log(pipeline);
     // console.log(keys);
     const keys_stringified = JSON.stringify(keys);
     let j1 = new Job("lint-check", "node");
