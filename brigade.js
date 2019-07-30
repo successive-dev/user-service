dec10d8d0e0d94c6cb9a6076ed43914afbf680c3
dev-successive <vishal.tewatia@successive.tech>
2019-07-30 14:25:26 +0530

    trigger

--- brigade.js
+++ brigade.js
@@ -17,7 +17,7 @@ docker.tasks = [
   "VERSION=1.5.0",
   "OS=linux",
   "ARCH=amd64",
-  "curl -fsSL 'https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz' \
+  "curl -fsSL `https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz` \
   // | tar xz --to-stdout ./docker-credential-gcr \
   // > /usr/bin/docker-credential-gcr && chmod +x /usr/bin/docker-credential-gcr",
   "echo ================================== login in ==========================================",
