1aeda84c3093d8f5ff62b6c00a9ab7a16b03e2be
dev-successive <vishal.tewatia@successive.tech>
2019-07-30 16:33:10 +0530

    trigger

--- brigade.js
+++ brigade.js
@@ -8,15 +8,20 @@ docker.privileged = true;
 docker.env = {
   DOCKER_DRIVER: driver
 }
+let VERSION = '1.5.0';
+let OS = 'linux';
+let ARCH = 'amd64';
+
+
 docker.tasks = [
   "dockerd-entrypoint.sh &",
   `sleep 30`,
   "docker info",
   "cd /src",
   "apk add curl",
-  "VERSION=1.5.0",
-  "OS=linux",
-  "ARCH=amd64",
+  // "VERSION=1.5.0",
+  // "OS=linux",
+  // "ARCH=amd64",
   `curl -fsSL "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" | tar xz --to-stdout ./docker-credential-gcr > /usr/bin/docker-credential-gcr && chmod +x /usr/bin/docker-credential-gcr`,
   "echo ================================== login in ==========================================",
   "docker-credential-gcr gcr-login > logs.txt",
